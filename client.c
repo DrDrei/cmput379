84bc3239814a6305546bceff93b1c0dc73803b2a
Andrei <drei3000@gmail.com>
2016-10-13 15:53:24 -0600

    client and server handshake working.

--- client.c
+++ client.c
@@ -7,51 +7,56 @@
 #include <stdlib.h>
 #include <strings.h>
 
-#define	 MY_PORT  2222
+#define	 MY_PORT  2234
 
 /* ---------------------------------------------------------------------
  This is a sample client program for the number server. The client and
  the server need not run on the same machine.				 
  --------------------------------------------------------------------- */
 
-int main()
-{
+int main() {
 	int	s, number;
-
 	struct	sockaddr_in	server;
-
 	struct	hostent		*host;
+	char * recieved[2];
 
 	host = gethostbyname ("localhost");
-
 	if (host == NULL) {
 		perror ("Client: cannot get host description");
 		exit (1);
 	}
 
-	while (1) {
+	s = socket (AF_INET, SOCK_STREAM, 0);
 
-		s = socket (AF_INET, SOCK_STREAM, 0);
-
-		if (s < 0) {
-			perror ("Client: cannot open socket");
-			exit (1);
-		}
-
-		bzero (&server, sizeof (server));
-		bcopy (host->h_addr, & (server.sin_addr), host->h_length);
-		server.sin_family = host->h_addrtype;
-		server.sin_port = htons (MY_PORT);
-
-		if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
-			perror ("Client: cannot connect to server");
-			exit (1);
-		}
-
-		read (s, &number, sizeof (number));
-		close (s);
-		fprintf (stderr, "Process %d gets number %d\n", getpid (),
-			ntohl (number));
-		sleep (5);
+	if (s < 0) {
+		perror ("Client: cannot open socket");
+		exit (1);
 	}
+
+	bzero(&server, sizeof (server));
+	bcopy(host->h_addr, &server.sin_addr, host->h_length);
+	server.sin_family = host->h_addrtype;
+	server.sin_port = htons (MY_PORT);
+
+	if (connect (s, (struct sockaddr*) & server, sizeof (server))) {
+		perror ("Client: cannot connect to server");
+		exit (1);
+	}
+
+	recv(s, &recieved, sizeof(recieved), 0);
+	if (recieved[0] == (char *) 0xCF && recieved[1] == (char *) 0xA7) {
+		fprintf (stderr, "Process %d \n", getpid());
+	}
+	// Connection Established, Messaging here.
+	while (1) {
+		break;	
+	}
+	close (s);	
 }
+
+
+
+
+
+
+
--- server.c
+++ server.c
@@ -5,8 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define	MY_PORT	2222
+#define	MY_PORT	2234
 
+
+// 
 /* ---------------------------------------------------------------------
  This	is  a sample server which opens a stream socket and then awaits
  requests coming from client processes. In response for a request, the
@@ -15,14 +17,18 @@
  chines.
  --------------------------------------------------------------------- */
 
-int main()
-{
+int main() {
 	int	sock, snew, fromlength, number, outnum;
-
 	struct	sockaddr_in	master, from;
+	
+	unsigned int userCount = 0;
 
 
-	sock = socket (AF_INET, SOCK_STREAM, 0);
+	char * handshake[2];
+	handshake[0] = (char *) 0xCF;
+	handshake[1] = (char *) 0xA7;
+
+ 	sock = socket (AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
 		perror ("Server: cannot open master socket");
 		exit (1);
@@ -32,25 +38,34 @@ int main()
 	master.sin_addr.s_addr = INADDR_ANY;
 	master.sin_port = htons (MY_PORT);
 
-	if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
+	if (bind (sock, (struct sockaddr*) &master, sizeof(master))) {
 		perror ("Server: cannot bind master socket");
 		exit (1);
 	}
 
 	number = 0;
 
-	listen (sock, 5);
+	listen(sock, 5);
 
 	while (1) {
 		fromlength = sizeof (from);
-		snew = accept (sock, (struct sockaddr*) & from, & fromlength);
+		snew = accept(sock, (struct sockaddr*) & from, & fromlength);
 		if (snew < 0) {
 			perror ("Server: accept failed");
 			exit (1);
 		}
-		outnum = htonl (number);
-		write (snew, &outnum, sizeof (outnum));
+		printf("Process %d \n", number);
+		// handshake = htonl(handshake);
+		userCount++;
+		send(snew, &handshake, sizeof(handshake)-1, 0);
 		close (snew);
 		number++;
 	}
 }
+
+
+
+
+
+
+
