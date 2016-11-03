b3e5de9ad002c2b521134ee1bd32565a0f531578
Andrei <drei3000@gmail.com>
2016-11-03 16:22:21 -0600

    Merge branch 'master' into backup
    # Conflicts:
    #	client.c

--- client.c
+++ client.c
@@ -1,33 +1,39 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+// might want to move this to the header file
+ // file descriptor for standard input
 // #define	 MY_PORT  2234
 
-/* ---------------------------------------------------------------------
- This is a sample client program for the number server. The client and
- the server need not run on the same machine.				 
- --------------------------------------------------------------------- */
+#include "sharedFunc.h"
+
+void intHandler(int sig);
+
+
+int s;
 
 // void signalHandler(int sig) {
 
 // }
 
 int main(int argc, char *argv[]) {
-	int	s, number;
+	int number;
+	//int	s, number;
 	struct	sockaddr_in	server;
 	struct	hostent		*host;
 
 	char buffer[256];
+	char message[256];
 	char * recieved[2];
 	char * username = "usernameUndefined";
+	int messageLength = 0;
 	int portname = 2222;
 	
+    struct timeval tv = {5, 0};
+    fd_set readfds;
+	FD_ZERO(&readfds);
+
+	//signal
+	signal(SIGINT, intHandler);
+
 	host = gethostbyname ("localhost");
 	if (argc == 3) {
 		portname = atoi(argv[1]);
@@ -56,31 +62,60 @@ int main(int argc, char *argv[]) {
 		exit (1);
 	}
 
-	
+	//handshake	
 	recv(s, &recieved, sizeof(recieved), 0);
-	// handshake
 	if (recieved[0] == (char *) 0xCF && recieved[1] == (char *) 0xA7) {
 		fprintf (stderr, "Your Process ID: %d \n", getpid());
-		
-	}
+		formatMessage(username, buffer); 
+		printf("Username length is: %d -- buffer: %s\n", messageLength, buffer+1);	
+		// sends the length followed by the username
+		send(s, buffer, sizeof(buffer)-1, 0);
 
-	// signal(SIGINT, signalHandler);
+	}
+	int checkSel;
 	// Connection Established, Messaging here.
 	while (1) { // only for child processes
-		printf("Enter your message: ");
-		bzero(buffer, 256);
-		fgets(buffer, 255, stdin);
-		fflush(stdout);
-		send(s, buffer, sizeof(buffer)-1, 0);
+        struct timeval tv = {5, 0}; // reset timer
+		FD_SET(STDIN, &readfds);
+        checkSel = select(STDIN+1, &readfds, NULL, NULL, &tv);
 		
+		if( checkSel < 0) {
+			perror("select");// error
+		} else if (FD_ISSET(STDIN, &readfds)) {
+			// we input our message here
+			// should we reset and initialize here as well?
+			memset(message, 0, sizeof(message));
+			fgets(message, 255, stdin);
+
+			formatMessage(message, buffer);
+			//printf("My message \n %s", buffer+1); // +1 ignores the first byte (pointer arithmetic)
+			send(s, buffer, sizeof(buffer)-1, 0);
+	
+        } else {
+            printf("Timed out.\n"); // every five seconds
+        }
+
 	}
 	close (s);	
 
 }
 
 
+void intHandler(int sig) {
+
+	char c;
+
+	signal(sig, SIG_IGN);
+	printf("You pressed ctrl-c?\n");
+	c = getchar();
+     if (c == 'y' || c == 'Y') {
+    	close(s);
+        exit(0);
+     } else {
+          signal(SIGINT, intHandler);
+     }
+     getchar();
 
 
-
-
+}
 
--- server.c
+++ server.c
@@ -1,20 +1,8 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
 
 // #define	MY_PORT	2234
 
-/* ---------------------------------------------------------------------
- This	is  a sample server which opens a stream socket and then awaits
- requests coming from client processes. In response for a request, the
- server sends an integer number  such	 that  different  processes  get
- distinct numbers. The server and the clients may run on different ma-
- chines.
- --------------------------------------------------------------------- */
+#include "sharedFunc.h"
 
 int main(int argc, char *argv[]) {
 	int	sock, snew, fromlength, number, outnum;
@@ -22,10 +10,27 @@ int main(int argc, char *argv[]) {
 	int portname = 2222;
 	
 	unsigned int userCount = 0;
+	unsigned int userCount2 = 0;
 	char buffer[256];
+	char username[256];
+	char updateMessage[256];
 	char * handshake[2];
 	handshake[0] = (char *) 0xCF;
 	handshake[1] = (char *) 0xA7;
+
+	char * userList[256];
+
+	// timming to cut connection
+
+	/*
+	*	Do we want to use a signal handler to stop the child 
+	*	process when connection is closed?
+	*
+	*/
+    struct timeval tv = {30, 0};
+    struct timeval tv2 = {5, 0};
+    fd_set readfds, readfds2;
+	//FD_ZERO(&readfds);
 
 	if (argc == 2) {
 		portname = atoi(argv[1]);
@@ -49,52 +54,124 @@ int main(int argc, char *argv[]) {
 	listen(sock, 5);
 
 	int pid; // used to fork process
-	static int counter = 0; // keep count of processes
-	int fd[2]; // file descriptor
+	int fd[2], fd2[2]; // file descriptors
 	int parentVal = 0;
 	pipe(fd);
+	int checkSel2;
 
 	while (1) {
 		fromlength = sizeof (from);
-		snew = accept(sock, (struct sockaddr*) &from, &fromlength);
+		while(1) {
+			struct timeval tv2 = {5, 0}; // reset timer
+			FD_SET(sock, &readfds2);
+	        checkSel2 = select(sock+1, &readfds2, NULL, NULL, &tv2);
+			
+			if( checkSel2 < 0) {
+				perror("select");// error
+			} else if (FD_ISSET(sock, &readfds2)) {
+
+				snew = accept(sock, (struct sockaddr*) &from, &fromlength);
+
+				break;
+	        } else {
+	        	// check if there is an update message
+	        	//read(fd[0], &updateMessage, sizeof(updateMessage));
+	        	printf("Time out %s\n", updateMessage+2);
+	            
+	         }
+
+		}
 		
 		if (snew < 0) {
 			perror ("Server: accept failed");
 			exit (1);
 		}
 
-		userCount++;
+		// send the handshake
+		memset(username, sizeof(username), 0);
+		send(snew, &handshake, sizeof(handshake)-1, 0); 
+		recv(snew, username, sizeof(username)-1, 0); // frecieve the username
 		
-		send(snew, &handshake, sizeof(handshake)-1, 0);
+		userList[userCount] = (username+1);
+
+		for(size_t i = 0; i < userCount; ++i) {
+			printf("%s\n", userList[i]);	
+
+		}
+		userCount++; // to get index of array
+		userCount2++; // actual count of users
 
 	    pid = fork();
-		printf("Return value of fork %d\n", pid);
-	    if ( pid  == -1) { // failure ... please never evaluate to true .... please
+		buffer[0] = 0x01; // new user has joined
+
+	    if ( pid  == -1) { // failure
 			close(snew);
 			continue;
 		} else if (pid > 0) {
-			close(snew); // we need this open? well not for my tests but later on...
-			++counter;
-			printf("I am a PARENT server with pid %d\n", getpid());
+
+			// this is the parent (main) server
+			close(snew); // we dont need the main server to be connected
 			
-			parentVal = 100;
-			write(fd[1], &parentVal, sizeof(parentVal)); // not through socket but pipe
+			// this is for user joining
+			memcpy(&updateMessage[1], username, sizeof(updateMessage));
+			write(fd[1], &updateMessage, sizeof(updateMessage));
 		} else if (pid == 0) {
-			++counter;
 			printf("I am a CHILD server with pid %d\n", getpid());
 			
-			read(fd[0], &parentVal, sizeof(parentVal));
-			printf("Parent has sent a message %d\n", parentVal);
+			read(fd[0], &updateMessage, sizeof(updateMessage));
+			printf("Parent has sent a message %s\n", updateMessage+2);
+
 		}
 
+
+		/*
+		* in the while loop for the child process
+		*
+		*/
+		int connected = 1;
+		int recvCheck, checkSel;
 		printf("User count is:%d\n", userCount);
 
 		while(!pid) {
-			memset(buffer, 256, 0);
-			recv(snew, buffer, sizeof(buffer), 0);
-		
-			printf("Server with pid %d Recieved Message: \n", getpid());
-			printf("%s", buffer);
+
+			struct timeval tv = {20, 0}; // reset timer
+			FD_SET(snew, &readfds);
+			checkSel = select(snew+1, &readfds, NULL, NULL, &tv);
+			if( checkSel < 0) {
+				perror("select");
+
+			} else if (FD_ISSET(snew, &readfds)) {
+				
+				memset(buffer, 0, 255);
+				recvCheck = recv(snew, buffer, sizeof(buffer), 0);
+
+				// keep this??? if we ctrl-c it will keep printing 0 otherwise
+				if(buffer[0] == 0) { continue; }
+
+				//printf("recvCheck is %d\n", recvCheck);
+				// testing	
+				printf("Server with pid %d Recieved Message: \n", getpid());
+				// there is a god dammed return carriage here. im thinking we 
+				// user buffer[0] to find it and set that byte to something else
+				memset(buffer+ (int)buffer[0] , 0, 1); // sets the byte after the message to 0
+				printf("%s  -- Length: %d\n", buffer+1, (int) buffer[0]); // buffer+1 ignores first byte
+				
+				write(fd2[1], &buffer, sizeof(buffer)); // write client message to pipe
+
+			} else {
+				printf("I have exited\n");
+				fflush(0);
+
+				memset(updateMessage + (int)buffer[0], 0, 1);
+				//memset(updateMessage, 2, 1);
+				write(fd2[1], &buffer, sizeof(buffer)); // write client message to pipe
+
+				//printf("Update Message closing: %d, %s\n", updateMessage[0], updateMessage+2);
+				close(snew);
+				exit(0);
+			}
+
+			
 		}
 
 		close (snew);
